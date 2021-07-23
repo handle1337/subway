f39ddd0eb82d9e376a5bd2ffa27df018309f7fe1
Synchro <mewsiof@gmail.com>
2021-07-23 09:46:56 +0000

    Update everything
    I basically remade this whole thing, now it works somewhat well. Output is extremely wonky.

--- subway.py
+++ subway.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import requests
+import re
 import sys
 import os
 import threading
@@ -8,6 +9,7 @@ import tempfile
 import colorama
 from bs4 import BeautifulSoup
 from argparse import *
+from datetime import datetime
 
 
 
@@ -23,33 +25,62 @@ _|\"\"\"\"\"\|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_| \"\"\"\"|
 """
 
 
-url = ""
+HTML_OBJECTS =	{
+	"a": "href",
+	"applet": ["archive","codebase"],
+	"area": "href",
+	"base": "href",
+	"blockquote": "cite",
+	"body": "background",
+	"del": "cite",
+	"form": "action",
+	"frame": ["src","longdesc"],
+	"head": "profile",
+	"iframe": ["src","longdesc"],
+	"img": ["src","srcset","longdesc","usemap"],
+	"input": ["src","usemap"],
+	"ins": "cite",
+	"link": "href",
+	"object": ["classid","codebase","data","usemap"],
+	"q": "cite",
+	"script": "src",
+	"audio": "src",
+	"button": "formaction",
+	"command": "icon",
+	"embed": "src",
+	"html": "manifest",
+	"input": "formaction",
+	"source": "src",
+	"track": "src",
+	"video": ["poster","src"],
+	"source": "srcset",
+	"meta": "content",
+}
 
+def parse_url(url):
+	if url.startswith('http'):
+		url = re.sub(r'https?://', '', url)
+	if url.startswith('www.'):
+		url = re.sub(r'www.', '', url)
+	return url
 
-
-def fuzz(file, output="output_enum.txt"):
-	with open(file, 'r') as f:
-		for line in f:
-			data = check_status(URL + "/" + line)
-			if output:
-				save_output(output, data[1])
-
-
-# TODO: make this pull subdomains from the virustotal api
-def enum(file, output="output_enum.txt"):
-	pass
-
-
-
-def recursive_fuzz(file, tmpfile, depth):
-	url_l = url
-	for i in range(depth):
-		for line in file:
-			if check_status(url_l + "/" + line):
-				tmpfile.write(check_status[1] + "\n")
-				url_l = url_l + "/" + line
-				file = tmpfile
-
+# TODO: make this pull subdomains from various apis
+def enum(wordlist, output_file, url):
+	try:
+		url = parse_url(url)
+		f = open(wordlist, 'r')
+		lines = f.readlines()
+		for line in lines:
+			sub = check_status(f"{line.strip()}.{url}")
+			if sub:
+				if output_file:
+					save_output(output_file, sub)
+				else:
+					pass
+			else:
+				pass
+	except Exception as e:
+		print(f"[ERROR] {str(e)}")
 
 
 def recursive_enum(file, tmpfile, depth):
@@ -61,8 +92,7 @@ def recursive_enum(file, tmpfile, depth):
 				tmpfile.write(check_status[1] + "\n")
 				url_l = url_l + "/" + line
 				file = tmpfile
-				#Theres a massive logic flaw in this function i need to test it
-
+				#Theres a massive logic flaw in this function, do not use until fixed
 
 
 def save_output(file, data):
@@ -70,21 +100,24 @@ def save_output(file, data):
 		f.write(data + "\n")
 
 
-
-def s_request(url_l):
-	r = requests.get(url_l)
-	return r.text
-
-
-
-def check_status(subdomain, directory):
-	url_l = subdomain + URL + "/" + directory
-	r_unicode = s_request(url_l)
-	title = r_unicode[r_unicode.find('<title>') + 7 : r_unicode.find('</title>')]
-	if r.status_code < 400:
+def check_status(url_l):
+	try:
+		print(f"[*]Currently trying: {url_l}", end='\r')
+		r = requests.get(f"http://{url_l}", timeout=5)
+	except requests.ConnectionError:
+		pass
+	else:
+		r = requests.get(f"http://{url_l}")
+		r_unicode = r.text
+		title = r_unicode[r_unicode.find('<title>') + 7 : r_unicode.find('</title>')]
+		r.close()
 		print(f"[{title}][{r.status_code}] {url_l}")
-		return [subdomain, directory]
-	return None
+		return f"[{title}][{r.status_code}] {url_l}"
+
+def print_info(url_l, wordlist_l, output):
+	text_break = "\n" + ("="*50) + "\n"
+
+	print(f"{text_break}Started @ [{datetime.now()}]\nHostname: {url_l}\nVersion: 0.1\nWordlist: {wordlist_l}\nOuput file: {output}{text_break}")
 
 
 def main():
@@ -93,65 +126,70 @@ def main():
 	parser = ArgumentParser(description="Enumerate subdomains and URLs",
 							usage="use '%(prog)s --help' for more information",
 							formatter_class=RawTextHelpFormatter)
-	parser.add_argument("--banner", "-b", action='store_true', help="Print banner at program startup")
+	parser.add_argument("--banner", "-b", help="disable banner")
 	parser.add_argument("--url", "-u", required=True, help="Target URL")
-	parser.add_argument("--output", "-o", nargs='+', dest="output", help="Output file", type=FileType('w'))
-	parser.add_argument("--dwordlist", "-dw", nargs='?', default=sys.stdin, type=FileType('r'),
-						help="Directory wordlist")
-	parser.add_argument("--swordlist", "-sw", nargs='?', default=sys.stdin, type=FileType('r'),
+
+	parser.add_argument("--output", "-o", dest="output", help="Output file")
+	parser.add_argument("--wordlist", "-w", default=None,
 						help="Subdomain wordlist")
-	parser.add_argument("--multisub", "-ms", help="Multilevel subdomain enumeration")
-	parser.add_argument("--recdir", "-rd", help="Recurisve directory fuzzing")
-	parser.add_argument("--crawl", "-c", action='store_true', help="Crawl source for URLs")
-	parser.add_argument("--outurl", "-ou", action='store_true', help="Output ALL found URLs")
+
+	parser.add_argument("--multisub", "-ms", help="multilevel subdomain enumeration")
+	parser.add_argument("--crawl", "-c", action='store_true', help="crawl source for URLs")
+	parser.add_argument("--outurl", "-ou", action='store_true', help="output ALL found URLs")
 	parser.add_argument("--inurl", "-iu", action='store_true',
-						help="[SUBDOMAIN ENUMERATION] Include URLs that are found inside the site's source [href]")
+						help="include URLs that are found inside the site's source [href]")
 	parser.add_argument("--inaurl", "-iau", action='store_true',
-						help="[SUBDOMAIN ENUMERATION] Include ALL found URLs in the site's source [href]")
+						help="include ALL found URLs in the site's source [href]")
 	args = parser.parse_args()
 	
 	
 	if args.banner:
+		pass
+	else:
 		print(BANNER)
-	if args.url:
-		url = args.url
+
+	url = args.url
+	wordlist = args.wordlist
+	output = args.output
+	print_info(url, wordlist, output)
+
+	try:
+		enum(wordlist, output, url)
+	except KeyboardInterrupt as e:
+		print(f"\n\nYou have exited the program...")
+	
 	# ======================================================================
-	# The purpose of this code is to save time by fuzzing directories and
-	# subdomains recursively by using process of elimination to create
+	# The purpose of this code is to save time by fuzzing subdomains
+	# recursively by using process of elimination to create
 	# a new wordlist to be used in the next iteration/depth
 	# ======================================================================
-	if args.multisub:
-		ms = tempfile.mkstemp()
-		with os.fdopen(ms, 'w') as mtmp:
-				with open(args.swordlist) as tmp:
-					pass
-		os.remove(ms)
+	
 
-	if args.recdir:
-		dd = tempfile.mkstemp()
-		with os.fdopen(dd, 'w') as rtmp:
-			with open(args.dwordlist) as dlist:
-				recursive_fuzz(dlist, rtmp, args.recdir[1])
-		os.remove(dd)
+
+"""	if args.multisub:
+		temp_wordlist = tempfile.mkstemp()
+		with os.fdopen(temp_wordlist, 'w') as tmp_f:
+				with open(wordlist) as wordlist_f:
+					recursive_enum(wordlist_f, tmp_f, args.multisub[1])
+		os.remove(temp_wordlist)
 
 	# ======================================================================
 
 	if args.crawl:
-		#ok i actually gotta loop through all dirs and subdomains and then their dirs, this code is gonna be extra fucky
 		r = s_request(url)
 		for link in BeautifulSoup(r, parse_only=SoupStrainer('a')):
-			if link.has_attr('href'):
-				print(link['href'])
-				if args.outurl:
-					pass
-				if args.inurl:
-					pass
-				if args.inaurl:
-					pass
+			for i in link:
+				if link.has_attr('href'):
+					print(link['href'])
+					if args.outurl:
+						pass
+					if args.inurl:
+						pass
+					if args.inaurl:
+						pass
+						"""
 
 	#TODO: multithread this shit
-	fuzz(args.dwordlist[0], args.output[0])
-	enum(args.swordlist[0], args.output[1])
 
 if __name__ == "__main__":
 	main()
