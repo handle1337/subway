36ec2dac65ee3b52ccec2bbf51c05386468f813a
0xHornet <mewsiof@gmail.com>
2021-07-30 03:26:37 +0000

    Fix output file bug

--- subway.py
+++ subway.py
@@ -13,6 +13,8 @@ from datetime import datetime
 
 
 
+# TODO: make this pull subdomains from various apis
+# TODO: build a DNS zone file based on the information
 
 BANNER = """
    ___            _                        _  _  
@@ -64,45 +66,42 @@ def parse_url(url):
 		url = re.sub(r'www.', '', url)
 	return url
 
-# TODO: make this pull subdomains from various apis
-def enum(wordlist, output_file, url):
+
+def bruteforce(wordlist, output_file, url):
 	try:
 		url = parse_url(url)
-		f = open(wordlist, 'r')
-		lines = f.readlines()
+		f_wordlist = open(wordlist, 'r')
+		f_output = open(output_file, 'w')
+		lines = f_wordlist.readlines()
 		for line in lines:
 			sub = check_status(f"{line.strip()}.{url}")
 			if sub:
 				if output_file:
-					save_output(output_file, sub)
-				else:
-					pass
-			else:
-				pass
+					f_output.write(sub + '\n')
+		f_wordlist.close()
+		f_output.close()
 	except Exception as e:
 		print(f"[ERROR] {str(e)}")
 
 
-def recursive_enum(file, tmpfile, depth):
+"""def recursive_bruteforce(file, tmpfile, depth):
 	url_l = url
 	for i in range(depth):
 		for line in file:
 			url_l = line + "." + url_l
 			if check_status(url_l):
-				tmpfile.write(check_status[1] + "\n")
+				tmpfile.write(check_status() + "\n")
 				url_l = url_l + "/" + line
 				file = tmpfile
 				#Theres a massive logic flaw in this function, do not use until fixed
-
-
-def save_output(file, data):
-	with open(file, 'w') as f:
-		f.write(data + "\n")
+				"""
 
 
 def check_status(url_l):
 	try:
-		print(f"[*]Currently trying: {url_l}", end='\r')
+		print(f"[*]Currently trying: {url_l}")
+		sys.stdout.write("\033[F")
+		sys.stdout.write("\033[K")
 		r = requests.get(f"http://{url_l}", timeout=5)
 	except requests.ConnectionError:
 		pass
@@ -111,8 +110,8 @@ def check_status(url_l):
 		r_unicode = r.text
 		title = r_unicode[r_unicode.find('<title>') + 7 : r_unicode.find('</title>')]
 		r.close()
-		print(f"[{title}][{r.status_code}] {url_l}")
-		return f"[{title}][{r.status_code}] {url_l}"
+		print(f"[{url_l}][{title}][{r.status_code}]")
+		return url_l
 
 def print_info(url_l, wordlist_l, output):
 	text_break = "\n" + ("="*50) + "\n"
@@ -126,7 +125,7 @@ def main():
 	parser = ArgumentParser(description="Enumerate subdomains and URLs",
 							usage="use '%(prog)s --help' for more information",
 							formatter_class=RawTextHelpFormatter)
-	parser.add_argument("--banner", "-b", help="disable banner")
+	parser.add_argument("--banner", "-b", action='store_true', help="disable banner")
 	parser.add_argument("--url", "-u", required=True, help="Target URL")
 
 	parser.add_argument("--output", "-o", dest="output", help="Output file")
@@ -154,8 +153,8 @@ def main():
 	print_info(url, wordlist, output)
 
 	try:
-		enum(wordlist, output, url)
-	except KeyboardInterrupt as e:
+		bruteforce(wordlist, output, url)
+	except KeyboardInterrupt:
 		print(f"\n\nYou have exited the program...")
 	
 	# ======================================================================
@@ -170,10 +169,12 @@ def main():
 		temp_wordlist = tempfile.mkstemp()
 		with os.fdopen(temp_wordlist, 'w') as tmp_f:
 				with open(wordlist) as wordlist_f:
-					recursive_enum(wordlist_f, tmp_f, args.multisub[1])
+					recursive_bruteforce(wordlist_f, tmp_f, args.multisub[1])
 		os.remove(temp_wordlist)
 
 	# ======================================================================
+
+	
 
 	if args.crawl:
 		r = s_request(url)
